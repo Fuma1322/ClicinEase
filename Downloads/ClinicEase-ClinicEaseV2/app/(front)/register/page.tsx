56f1020cb3fcbfdf3b9c5c577763b17acade2024
Fuma1322 <leonardfuma26@gmail.com>
2024-07-04 16:39:30 +0200

    sleepless

---
+++ Downloads/ClinicEase-ClinicEaseV2/app/(front)/register/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '@components/Auth/LoginForm'
+import RegisterForm from '@components/Auth/RegisterForm'
+import RegisterFormWitBg from '@components/Auth/Register'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="">
+      <RegisterFormWitBg />
+    </div>
+  )
+}
---
+++ Downloads/ClinicEase-ClinicEaseV2/app/(front)/verify-account/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { getUserById } from "@/actions/users";
+import VerifyTokenForm from "@/components/Frontend/VerifyTokenForm";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+ 
+export default async function VerifyAccount({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  //Get a User
+  const user = await getUserById(id);
+  const userToken = user?.token;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Card className="mx-auto max-w-md">
+      <CardHeader>
+        <CardTitle className="text-xl">Verify Token</CardTitle>
+        <CardDescription>
+          Please enter the 6-figure pass code sent to your email - {user?.email}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <VerifyTokenForm userToken={userToken} id={id} />
+      </CardContent>
+    </Card>
+    </div>
+
+  );
+}
+  
