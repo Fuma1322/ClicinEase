bc2ebf368736676a60a27821643e1265966da826
Fuma1322 <leonardfuma26@gmail.com>
2024-07-10 17:04:56 +0200

    debugging 26

---
+++ app/(back)/dashboard/clinic/appointments/layout.tsx
@@ -0,0 +1,36 @@
+import { getDoctorAppointments } from '@/actions/appointments';
+import ListPannel from '@/components/Dashboard/Clinic/ListPannel';
+import PannelHeader from '@/components/Dashboard/Clinic/PannelHeader';
+import NotAuthorised from '@/components/NotAuthorised';
+import { authOptions } from '@/lib/auth';
+import { Calendar } from 'lucide-react';
+import { getServerSession } from 'next-auth';
+import React, { ReactNode } from 'react'
+
+export default async function AppointmentLayout({children}:{children: ReactNode;}) {
+    const session = await getServerSession(authOptions);
+    const user = session?.user
+    if (user?.role !=="DOCTOR"){
+      return (
+        <NotAuthorised/>
+      )
+    }
+    const appointments = (await getDoctorAppointments(user.id)).data || [];
+    return (
+    <div>
+      
+    <div className="grid grid-cols-12">
+    <div className="col-span-4 py-3 border-r border-gray-100">
+     <PannelHeader title='Appointments' count={appointments.length??0} icon={Calendar}/>
+    <div className="px-3">
+    <ListPannel appointments={appointments} role={user?.role}/>
+    </div>
+    </div>
+     <div className="col-span-8"> 
+         {children}
+     </div>
+    </div>
+     
+ </div>
+  );
+}
--- app/(back)/dashboard/clinic/appointments/page.tsx
+++ app/(back)/dashboard/clinic/appointments/page.tsx
@@ -1,12 +1,9 @@
-import Appointments from '@/components/Dashboard/Appointments/Appointments'
-import DisplayPannel from '@/components/Dashboard/Clinic/DisplayPannel'
+import { getDoctorAppointments } from '@/actions/appointments'
 import ListPannel from '@/components/Dashboard/Clinic/ListPannel'
-import NewButton from '@/components/Dashboard/Clinic/NewButton'
-import PannelHeader from '@/components/Dashboard/Clinic/PannelHeader'
-import { Calendar } from 'lucide-react'
 import React from 'react'
 
-export default function page() {
+export default async function page() {}
+  const appointments = (await getDoctorAppointments(user.id)).data || [];
   return (
     <div>
       
@@ -14,13 +11,13 @@ export default function page() {
        <div className="col-span-4 py-3 border-r border-gray-100">
         <PannelHeader title='Appointments' count={appointments.length} icon={Calendar}/>
        <div className="px-3">
-       <ListPannel appointments={[]} role={''}/>
+       <ListPannel appointments={appointments} role={''}/>
        </div>
        </div>
         <div className="col-span-8">
             <div className="py-2 px-4 border-b border-gray-200 flex items-center justify-end">
                 <div className="flex items-center gap-4">
-                    <NewButton title='New Apointment' href='#'/>
+                    <NewButton title='New Appointment' href='/dashboard/doctor/appointments'/>
       </div>
     </div>
           <DisplayPannel count={0} newAppointmentLink={''} title={''}/>
